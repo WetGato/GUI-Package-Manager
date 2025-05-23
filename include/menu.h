8ef70f55c7cce214a2bff661d33bd0d520b11054
FastPlayer1234 <tinyplutt3@gmail.com>
2025-05-23 13:38:03 -0400

    Added support for updating all packages

--- include/menu.h
+++ include/menu.h
@@ -11,6 +11,7 @@ namespace MenuBuilder {
 const int ID_QueryPackages = 1;
 const int ID_ABOUT = 2;
 const int ID_ListUpgradeable = 3;
+const int ID_UPDATE = 4;
 
 
 #endif //MENU_H
--- menu.cpp
+++ menu.cpp
@@ -23,6 +23,7 @@ wxMenuBar *MenuBuilder::CreateMenuBar(wxFrame *parent)
             queryManagePackageMenu->Append(ID_QueryPackages, "List installed\tCtrl-L", "List all installed packages");
         managePackageMenu->AppendSubMenu(updatePackageMenu, "Update");
             updatePackageMenu->Append(ID_ListUpgradeable, "List Upgradeable", "List the upgradeable packages");
+            updatePackageMenu->Append(ID_UPDATE, "Update all packages", "Update all packages with available updates");
 
     menuBar->Append(helpMenu, "&Help");
         helpMenu->Append(ID_ABOUT, "About", "About this program");
